92f7ae358c6ace59179e847ef5ce687b8801a031
endorphin82 <endorphin82@gmail.com>
2019-10-31 11:23:09 +0200

    04.with_filters

--- src/components/Tickets/index.js
+++ src/components/Tickets/index.js
@@ -2,15 +2,15 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Ticket from "./Ticket";
 import { loadAllTickets } from "../../AC";
+import { ticketsGetter } from "../../selectors";
 
 const Tickets = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadAllTickets());
   }, [dispatch]);
-  const { tickets } = useSelector(
-    state => ({ tickets: state.tickets.tickets.sort((a, b) => (a.stops > b.stops) ? 1 : ((b.stops > a.stops) ? -1 : 0)) })
-  );
+  const { tickets } = useSelector( ticketsGetter );
+
   return (
     <>
       <h2>Tickets</h2>
